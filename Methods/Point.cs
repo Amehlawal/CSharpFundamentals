c603b2f6c77345dd01867f130d8ae7587ecaf645
Ameh Lawal <amehlawal@gmail.com>
2024-04-29 10:41:13 +0100

    Add project files.

---
+++ Methods/Point.cs
@@ -0,0 +1,27 @@
+﻿namespace Methods
+{
+    internal partial class Program
+    {
+        public class Point
+        {
+            public int x;
+            public int y;
+
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            public void Move(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            public void Move(Point p)
+            {
+                this.x = p.x;
+                this.y = p.y;
+            }
+        }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Methods
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = new Point(10, 20);
+            point.Move(new Point(10, 5));
+              Console.WriteLine();
+        }
+    }
+}
