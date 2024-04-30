207e564bc17eb8ab600b5e3def84af22c0e16999
Ameh Lawal <amehlawal@gmail.com>
2024-04-30 10:14:27 +0100

    learn about input and type conversion

---
+++ Input/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What's your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Hello I am " + age + " years old");
+        }
+    }
+}
