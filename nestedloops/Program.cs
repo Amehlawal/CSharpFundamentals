bf22383f285da4aea9927494d5b98e83f3ab3b13
Ameh Lawal <amehlawal@gmail.com>
2024-09-07 08:14:21 +0100

    nested loops

---
+++ nestedloops/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("How many rows ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("How many columns ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("what symbol ");
+string symbol = Console.ReadLine();
+
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write(symbol);
+    }
+    Console.WriteLine();
+}
