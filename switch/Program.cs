52c44cf437f25a04b6b281337132ec01da38b267
Ameh Lawal <amehlawal@gmail.com>
2024-09-07 07:37:33 +0100

    writing better if statement with switch

---
+++ switch/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! What is today: ");
+
+string day = Console.ReadLine();
+
+
+
+switch(day)
+{
+    case "Monday":
+        Console.WriteLine("It is monday");
+        break;
+    case "Tuesday":
+        Console.WriteLine("It is Tuesday");
+        break;
+    case "Wednesday":
+        Console.WriteLine("It is wednesday");
+        break;
+    case "Friday":
+        Console.WriteLine("It is friday");
+        break;
+    default:
+        Console.WriteLine("holla");
+        break;
+}
