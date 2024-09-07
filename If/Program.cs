e9a3071eb94463af82365d962c8cac9d68726603
Ameh Lawal <amehlawal@gmail.com>
2024-09-07 07:31:17 +0100

    learnt if and string methods

---
+++ If/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Please enter your age ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+if (age > 10)
+{
+    Console.WriteLine("You loser");
+}
+else if (age > 20)
+{
+    Console.WriteLine("You are a winner");
+}
+else
+{
+    Console.WriteLine("get out of my face");
+}
+
+Console.ReadKey();
---
+++ String/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string fullname = "Lawal";
+
+string phoneNumber = "123-456-789";
+
+phoneNumber = phoneNumber.Replace("-", "");
+
+Console.WriteLine(phoneNumber);
+
+string userName = fullname.Insert(0, "@");
+
+string shortLawal = fullname.Substring(0, 2);
+
+Console.WriteLine(shortLawal);
+
+Console.WriteLine(fullname.Length);
+
+Console.WriteLine(userName);
