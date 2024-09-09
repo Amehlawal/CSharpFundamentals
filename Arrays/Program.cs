2bd6a55b03d9eabb94489f85d67c0a6aa442da65
Ameh Lawal <amehlawal@gmail.com>
2024-09-09 08:27:29 +0100

    method overloading

---
+++ Arrays/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] cars = { "BMW", "Mustang", "Converter" };
+
+Console.WriteLine(cars[0]);
+
+for(int i = 0;i < cars.Length; i++)
+{
+    Console.WriteLine(cars[i]);
+}
+
+foreach(string car in cars)
+{
+    Console.WriteLine(car);
+}
---
+++ Method/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            double x;
+            double y;
+
+            double result;
+
+            Console.WriteLine("Enter in number 1: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Enter in number 2: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            result = Multiply(x, y);
+
+            Console.WriteLine(result);
+
+            double buy = CheckOut(2, 3, 4, 4, 5);
+            Console.WriteLine( buy);
+
+
+        }
+        static double Multiply(double x, double y)
+        {
+            double z = x * y;
+
+            return z;
+        }
+        static double CheckOut(params double[] z)
+        {
+           double total = 0;
+            foreach (double x in z)
+            {
+                total += x;
+            }
+
+            return total;
+        }
+    }
+}
