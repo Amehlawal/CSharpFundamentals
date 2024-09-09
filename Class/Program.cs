19a26521ddf8a39e836e83860d505404add2f66c
Ameh Lawal <amehlawal@gmail.com>
2024-09-09 12:09:53 +0100

    inheritance

--- Class/Program.cs
+++ Class/Program.cs
@@ -1,10 +1,34 @@
 ﻿namespace Class
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            //Car car1 = new Car("Lambo", 12);
+            Car car = new Car();
+            Console.WriteLine(car.wheels);
+            car.go();
         }
+
+        
     }
+    class Vehicle
+    {
+        public int speed = 0;
+        public void go()
+        {
+            Console.WriteLine("This vehicle is moving");
+        }
+        
+  
+   
+    }
+    class Car : Vehicle
+    {
+        public int wheels = 4;
+
+    }
+
 }
